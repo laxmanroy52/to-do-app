7b559f73917947cee449d623639211ee4d94a73a
Laxman Roy <88562390+laxmanroy52@users.noreply.github.com>
2023-06-04 21:27:14 +0600

    Solve all issue

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ let getText = document.getElementsByTagName('input')[0];
 let getDate = document.getElementsByTagName('input')[1];
 let create = document.getElementById('button');
 let main = document.getElementsByTagName('main')[0];
+let list = document.getElementsByClassName('list');
 let rem = document.querySelector('.rem');
 let notice = document.querySelector('.notice');
 let startX, startY, offsetX = 0, offsetY = 0;
@@ -17,15 +18,38 @@ let date, time, second;
 let count = 0;
 let select;
 
+
+//set initial time
+getDate.value = new Date().toLocaleTimeString().slice(0, -3);
+//double text not allowed
+let doubleText = function() {
+  let x = 0;
+  let val = getText.value.trim().replace((/\s+/gm), ' ');
+  let num, con;
+  num = list.length;
+  num = (num == 0) ? 1 : num;
+  setDone: while (x < num) {
+    if (list[x]) {
+    con = list[x].firstElementChild.innerText.trim().replace((/\s+/gm), ' ') != val;
+      if (!con) {
+        break setDone;
+      }
+    } else {
+      con = true;
+    }
+    x++;
+  }
+  return con;
+  }
 //new task
-create.addEventListener('click', function() {
+function button(e) {
   //blank text or white space disallowed
- if (getText.value.replace(/\s+/gm, '') != '') {
+ if (getText.value.trim().replace(/\s+/gm, '') != '' && doubleText()) {
   count++;
   todo.parentNode.style.height = '80px';
   //create new task
   let taskElement = document.createElement('div');
-  taskElement.id = 'list';
+  taskElement.classList.add('list');
   taskElement.innerHTML = `
       <div class="text" onclick="see(this)">${getText.value}</div>
       <div class="date">${getDate.value}</div>
@@ -40,20 +64,23 @@ create.addEventListener('click', function() {
   header.style = 'height: 50px; border-bottom-width: 2px; justify-content: space-around';
   logo.style.fontSize = '100%';
   getText.value = null;
-  getDate.value = '12:00';
   // Wait for the next frame to apply the transition
   taskElement.style.animation = 'expandTask 0.2s forwards';
+  //set button animation
+  e.style.scale = '.9';
+  setTimeout(function(){
+    e.style.scale = '1';
+  },200);
+  
   select = document.querySelectorAll('.date');
- }
-})
-
+  }
+}
 // ellipsis text view as notice
 function see(e) {
   let text = e.parentNode.parentNode.children[0].lastElementChild.children[2];
     e.parentNode.parentNode.firstElementChild.style = `top: ${e.parentNode.offsetTop + e.parentNode.offsetHeight + 15}px`;
      text.innerText = e.innerText;
-     
-     if ( text.offsetHeight <= 30) {
+     if (text.offsetHeight <= 30) {
      text.style.lineHeight = '30px';
      } else {
        text.style.lineHeight = '20px';
@@ -110,7 +137,6 @@ function del(e) {
 function cross(e) {
   e.parentNode.parentNode.style.top = -(e.parentNode.parentNode.offsetHeight + 15) + 'px';
 }
-
 //when time come task done and vibrate
 setInterval(function(){
   date = new Date();
@@ -124,4 +150,10 @@ setInterval(function(){
   navigator.vibrate(300);
 }
 }
-}, 1000);
+    
+    if (getText.value.trim().replace(/\s+/gm, ' ').length > 0 && doubleText()) {
+      create.style.color = 'white';
+    } else {
+        create.style.color = 'rgba(0,0,0,.4)';
+    }
+}, 100);
